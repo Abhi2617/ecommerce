d30dbc52e0edf9ed1a0c0751e7c4fee4bd7750f5
Abhi2617 <abhidevsunil17@gmail.com>
2025-07-22 18:55:24 +0530

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function addToCart() {
   cartCount++;
   document.getElementById("cart-count").textContent = cartCount;
 }
+function searchProducts() {
+  const input = document.getElementById("search-input").value.toLowerCase();
+  const products = document.querySelectorAll(".product");
+
+  products.forEach(product => {
+    const title = product.querySelector("h3").textContent.toLowerCase();
+    if (title.includes(input)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+}
